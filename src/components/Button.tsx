074783417a83e2bae51834fc8b70a358ccb6b6b0
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-09-01 11:53:19 +1000

    moved all button styles to classnames

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,14 +11,13 @@ const Button: React.FC<Props> = (props) => {
   const { title, color, onClick } = props;
 
   const buttonStyles = classnames({
+    "py-2 px-3 font-bold rounded-md hover:scale-[1.15] duration-200 transition-transform":
+      true,
     "bg-idem-yellow text-black": color == "yellow"
   });
 
   return (
-    <button
-      className={`py-2 px-3 font-bold rounded-md hover:scale-[1.15] duration-200 transition-transform ${buttonStyles}`}
-      onClick={onClick}
-    >
+    <button className={buttonStyles} onClick={onClick}>
       {title}
     </button>
   );
