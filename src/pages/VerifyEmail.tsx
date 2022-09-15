bc9a51978f5db3909cb463da53b4d3246ade2120
Brett van Niekerk <bvnd42@gmail.com>
2022-09-15 15:59:56 +1000

    Removed draggable

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Draggable from "react-draggable";
 import api from "../apis/Api";
 
 type Error = {
@@ -33,41 +32,39 @@ const VerifyEmail: React.FC = () => {
   return (
     <div className="min-w-screen min-h-screen flex flex-col justify-start items-center bg-idem-black bg-hero-plus-idem-white-light box-border">
       <div className="w-full h-screen flex flex-col justify-center items-center">
-        <Draggable bounds="parent" handle=".handle">
-          <div className="w-[90vw] md:w-auto rounded-2xl shadow-2xl bg-idem-white bg-hero-topography flex flex-col justify-center items-center z-50">
-            <h1
-              className="px-12 py-4 text-center text-2xl md:text-3xl font-bold text-idem-white w-full bg-gradient-to-tl \
-              from-orange-500 to-yellow-400 rounded-t-2xl select-none handle xl:cursor-move"
-            >
-              VERIFY EMAIL
-            </h1>
-            <div className="px-4 md:px-12 py-3 md:py-8 text-center">
-              {token ? (
-                isVerifying ? (
-                  <h3 className="text-idem-black text-lg font-bold">
-                    Verifying...
-                  </h3>
-                ) : result ? (
-                  <p className="bg-gradient-to-tl from-orange-500 to-yellow-400 text-idem-white font-bold px-3 py-2 rounded-lg text-2xl shadow-lg">
-                    You have been successfully verified, welcome to IDEM
-                  </p>
-                ) : error ? (
-                  <p className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
-                    {error.message}
-                  </p>
-                ) : (
-                  <p className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
-                    Failed to verify token
-                  </p>
-                )
+        <div className="w-[90vw] md:w-auto rounded-2xl shadow-2xl bg-idem-white bg-hero-topography flex flex-col justify-center items-center z-50">
+          <h1
+            className="px-12 py-4 text-center text-2xl md:text-3xl font-bold text-idem-white w-full bg-gradient-to-tl \
+            from-orange-500 to-yellow-400 rounded-t-2xl select-none xl:cursor-move"
+          >
+            VERIFY EMAIL
+          </h1>
+          <div className="px-4 md:px-12 py-3 md:py-8 text-center">
+            {token ? (
+              isVerifying ? (
+                <h3 className="text-idem-black text-lg font-bold">
+                  Verifying...
+                </h3>
+              ) : result ? (
+                <p className="bg-gradient-to-tl from-orange-500 to-yellow-400 text-idem-white font-bold px-3 py-2 rounded-lg text-2xl shadow-lg">
+                  You have been successfully verified, welcome to IDEM
+                </p>
+              ) : error ? (
+                <p className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
+                  {error.message}
+                </p>
               ) : (
-                <span className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
-                  No Token provided
-                </span>
-              )}
-            </div>
+                <p className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
+                  Failed to verify token
+                </p>
+              )
+            ) : (
+              <span className="bg-red-600 text-idem-white px-3 py-2 rounded-lg text-lg">
+                No Token provided
+              </span>
+            )}
           </div>
-        </Draggable>
+        </div>
       </div>
       <div className="absolute w-full md:w-auto text-center bottom-0 md:top-0 md:left-0">
         <div
