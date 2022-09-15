dddd7d1f25d30e49afd649517094c692222b486d
Brett van Niekerk <bvnd42@gmail.com>
2022-09-15 15:48:42 +1000

    Suggested changes for IDEM-30

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="w-full bg-idem-yellow flex flex-col lg:flex-row justify-center items-center px-6 py-2 text-idem-black font-bold">
+    <div className="w-full bg-idem-yellow flex flex-col lg:flex-row justify-between items-center px-6 py-2 text-idem-black font-bold">
       <span>
         Copyright DLTx Labs |{" "}
         <a
@@ -12,8 +12,18 @@ const Footer: React.FC = () => {
           rel="noopener noreferrer"
         >
           PGP Public Key
-        </a>{" "}
-        | <a>0xB35662F7Ac34a62E122C77fE90BAb74443e71b00</a>
+        </a>
+      </span>
+      <span>
+        Checkout IDEM on:{" "}
+        <a
+          className="underline underline-offset-2 decoration-2"
+          href={`https://etherscan.io/address/${process.env.VITE_ETH_ADDRESS}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Etherscan
+        </a>
       </span>
     </div>
   );
