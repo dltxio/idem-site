8c7c0b3ed19ccc282a54ee780e9e61a01198f498
Alex Xiang <alex.xiang@dltx.io>
2022-10-06 13:44:02 +1000

    proxy end point changed

--- src/apis/Api.ts
+++ src/apis/Api.ts
@@ -2,7 +2,7 @@ import HTTPClient from "./HTTPClient";
 
 class Api extends HTTPClient {
   public verifyEmail = async (token: string) =>
-    this.post<boolean>(`user/verify-email`, { token: token });
+    this.post<boolean>(`users/verify-email`, { token: token });
 }
 
 const api = new Api(process.env.VITE_APP_PROXY_URL as string, true);
